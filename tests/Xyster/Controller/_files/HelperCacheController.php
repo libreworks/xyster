1ba4843e73c0ee2411bf858ed36cb14e82293a82
Jonathan Hawk <jonathan@libreworks.com>
2007-09-13 00:08:30 +0000

    Oops.  Forgot one.

---
+++ tests/Xyster/Controller/_files/HelperCacheController.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once 'Zend/Controller/Action.php';
+
+class HelperCacheController extends Zend_Controller_Action 
+{
+    
+    public function indexAction()
+    {
+        // nothing - we just need a controller
+    }
+    
+}
