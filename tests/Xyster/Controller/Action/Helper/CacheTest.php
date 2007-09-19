27ea8e5624a83decffbaa40288b03c9bc457a180
Jonathan Hawk <jonathan@libreworks.com>
2007-09-19 21:29:24 +0000

    resolves issue #8, resolves issue #9, resolves issue #10

--- tests/Xyster/Controller/Action/Helper/CacheTest.php
+++ tests/Xyster/Controller/Action/Helper/CacheTest.php
@@ -1,5 +1,24 @@
 <?php
-// Call Xyster_Controller_Action_Helper_CacheTest::main() if this source file is executed directly.
+/**
+ * Xyster Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.opensource.org/licenses/bsd-license.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to xyster@devweblog.org so we can send you a copy immediately.
+ *
+ * @category  Xyster
+ * @package   Xyster_Controller
+ * @subpackage   UnitTests
+ * @copyright Copyright (c) 2007 Irrational Logic (http://devweblog.org)
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @version   $Id$
+ */
 if (!defined('PHPUnit_MAIN_METHOD')) {
     define('PHPUnit_MAIN_METHOD', 'Xyster_Controller_Action_Helper_CacheTest::main');
 }
