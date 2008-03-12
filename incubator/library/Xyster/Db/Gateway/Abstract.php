77a1b81484951f6f6148eab5b8dc7f37216ea56f
Jonathan Hawk <jonathan@libreworks.com>
2008-03-12 23:44:10 +0000

    Adding Xyster_Db_Gateway folders and stub files

---
+++ incubator/library/Xyster/Db/Gateway/Abstract.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Xyster Framework
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.opensource.org/licenses/bsd-license.php
+ *
+ * @category  Xyster
+ * @package   Xyster_Db
+ * @copyright Copyright (c) 2007-2008 Irrational Logic (http://irrationallogic.net)
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @version   $Id$
+ */
+/**
+ * A gateway and abstraction layer for database administration
+ *
+ * @category  Xyster
+ * @package   Xyster_Db
+ * @copyright Copyright (c) 2007-2008 Irrational Logic (http://irrationallogic.net)
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ */
+abstract class Xyster_Db_Gateway_Abstract
+{
+	
+}
