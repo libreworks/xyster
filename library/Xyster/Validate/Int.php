2c7448d8b4c5169bad06c040a7604be7a6243c69
Rob Speed <speed.rob@gmail.com>
2008-06-07 20:46:28 +0000

    refs issue #43 Adds number verification that won't fail on 64-bit CPUs.

---
+++ library/Xyster/Validate/Int.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Xyster Framework
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.opensource.org/licenses/bsd-license.php
+ *
+ * @category  Xyster
+ * @package   Xyster_Validate
+ * @copyright Copyright (c) 2007-2008 Irrational Logic (http://irrationallogic.net)
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @version   $Id: Uri.php 190 2008-01-07 22:39:57Z doublecompile $
+ */
+/**
+ * Zend_Validate_Abstract
+ */
+require_once 'Zend/Validate/Abstract.php';
+/**
+ * A URI validator
+ *
+ * @category  Xyster
+ * @package   Xyster_Validate
+ * @copyright Copyright (c) 2007-2008 Irrational Logic (http://irrationallogic.net)
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ */
+class Xyster_Validate_Int extends Zend_Validate_Abstract
+{
+
+    const NOT_INT = 'notInt';
+
+    /**
+     * @var array
+     */
+    protected $_messageTemplates = array(
+        self::NOT_INT => "'%value%' does not appear to be an integer"
+    );
+
+    /**
+     * Defined by Zend_Validate_Interface
+     *
+     * Returns true if and only if $value is a valid integer
+     *
+     * @param  string $value
+     * @return boolean
+     */
+    public function isValid($value)
+    {
+        $valueString = (string) $value;
+
+        $this->_setValue($valueString);
+
+        $locale = localeconv();
+
+        $valueFiltered = str_replace($locale['decimal_point'], '.', $valueString);
+        $valueFiltered = str_replace($locale['thousands_sep'], '', $valueFiltered);
+
+        if ( preg_match( '/^[0-9]+$/', $valueFiltered ) ) {
+            $this->_error();
+            return false;
+        }
+
+        return true;
+    }
+
+}
