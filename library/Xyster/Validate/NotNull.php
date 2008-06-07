77b11eed026999b3b6c02a618949533cc2f3df22
Rob Speed <speed.rob@gmail.com>
2008-06-07 21:07:58 +0000

    refs issue #44 Adds validator for non-null values.

---
+++ library/Xyster/Validate/NotNull.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Xyster Framework
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.opensource.org/licenses/bsd-license.php
+ *
+ * @category  Xyster
+ * @package   Xyster_Validate
+ * @copyright Copyright (c) 2007-2008 Irrational Logic (http://irrationallogic.net)
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @version   $Id: Uri.php 190 2008-01-07 22:39:57Z doublecompile $
+ */
+/**
+ * Zend_Validate_NotEmpty
+ */
+require_once 'Zend/Validate/NotEmpty.php';
+/**
+ * A null value validator 
+ *
+ * @category  Xyster
+ * @package   Xyster_Validate
+ * @copyright Copyright (c) 2007-2008 Irrational Logic (http://irrationallogic.net)
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ */
+class Xyster_Validate_NotNull extends Zend_Validate_NotEmpty
+{
+
+    /**
+     * Defined by Zend_Validate_Interface
+     *
+     * Returns true if $value is not null.
+     *
+     * @param  string $value
+     * @return boolean
+     */
+    public function isValid($value)
+    {
+        $this->_setValue($value);
+
+        if ($value === NULL) {
+            $this->_error();
+            return false;
+        }
+
+        return true;
+    }
+
+}
