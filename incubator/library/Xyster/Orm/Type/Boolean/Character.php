4d196b14d890a120821b56664b528dedb6ace391
Rob Speed <speed.rob@gmail.com>
2008-09-27 01:42:53 +0000

    Created character boolean types.

---
+++ incubator/library/Xyster/Orm/Type/Boolean/Character.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Xyster Framework
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.opensource.org/licenses/bsd-license.php
+ *
+ * @category  Xyster
+ * @package   Xyster_Orm
+ * @copyright Copyright (c) 2007-2008 Irrational Logic (http://irrationallogic.net)
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @version   $Id$
+ */
+/**
+ * @see Xyster_Orm_Type_Boolean
+ */
+require_once 'Xyster/Orm/Type/Boolean.php';
+/**
+ * The boolean type
+ *
+ * @category  Xyster
+ * @package   Xyster_Orm
+ * @copyright Copyright (c) 2007-2008 Irrational Logic (http://irrationallogic.net)
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ */
+abstract class Xyster_Orm_Type_Boolean_Character extends Xyster_Orm_Type_Boolean
+{
+    /**
+     * Gets the underlying database type
+     *
+     * @return Xyster_Db_DataType
+     */
+    public function getDataType()
+    {
+        return Xyster_Db_DataType::Char();
+    }
+
+    /**
+     * Returns the string representing a false value.
+     *
+     * @return string
+     */
+    protected abstract function _getFalseString();
+
+    /**
+     * Returns the string representing a true value.
+     *
+     * @return string
+     */
+    protected abstract function _getTrueString();
+
+    /**
+     * Sets the value to the prepared statement
+     *
+     * @param Zend_Db_Statement_Interface $stmt The statment to set
+     * @param mixed $value The value to bind into the statement
+     * @param int $index The starting index to bind
+     * @param Xyster_Orm_Session_Interface $sess The ORM session
+     * @param array $settable Boolean for each column's settability
+     */
+    public function set(Zend_Db_Statement_Interface $stmt, $value, $index, Xyster_Orm_Session_Interface $sess, array $settable = array() )
+    {
+        if ( !count($settable) || $settable[0] ) {
+            $stmt->bindValue($index, $value, Zend_Db::PARAM_STR);
+        }
+    }   
+}
---
+++ incubator/library/Xyster/Orm/Type/Boolean/YesNo.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Xyster Framework
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.opensource.org/licenses/bsd-license.php
+ *
+ * @category  Xyster
+ * @package   Xyster_Orm
+ * @copyright Copyright (c) 2007-2008 Irrational Logic (http://irrationallogic.net)
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @version   $Id$
+ */
+/**
+ * @see Xyster_Orm_Type_Boolean
+ */
+require_once 'Xyster/Orm/Type/Boolean.php';
+/**
+ * The boolean type
+ *
+ * @category  Xyster
+ * @package   Xyster_Orm
+ * @copyright Copyright (c) 2007-2008 Irrational Logic (http://irrationallogic.net)
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ */
+abstract class Xyster_Orm_Type_Boolean_YesNo extends Xyster_Orm_Type_Boolean_Character
+{
+    /**
+     * Returns the abbreviated name of the type.
+      *
+      * @return string
+     */
+    public function getName()
+    {
+        return 'yes_no';
+    }
+
+    /**
+     * Returns the string representing a false value.
+     *
+     * @return string
+     */
+    protected function _getFalseString()
+    {
+        return 'no';
+    }
+
+    /**
+     * Returns the string representing a true value.
+     *
+     * @return string
+     */
+    protected function _getTrueString()
+    {
+        return 'yes';
+    }
+}
