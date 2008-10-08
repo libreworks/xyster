b8a7cbf0c2463cdd561cda4605f226f135ad513a
Jonathan Hawk <jonathan@libreworks.com>
2008-10-08 03:52:59 +0000

    Finish fleshing out the collectionentry

--- incubator/library/Xyster/Orm/Engine/CollectionEntry.php
+++ incubator/library/Xyster/Orm/Engine/CollectionEntry.php
@@ -15,6 +15,8 @@
  */
 /**
  * A collection value object
+ * 
+ * @todo This thing really needs to be cleaned up and improved.
  *
  * @category  Xyster
  * @package   Xyster_Orm
@@ -23,34 +25,99 @@
  */
 class Xyster_Orm_Engine_CollectionEntry
 {
-    //ATTRIBUTES MAINTAINED BETWEEN FLUSH CYCLES
-	
-    // session-start/post-flush persistent state
     private $_snapshot;
-    // allow the CollectionSnapshot to be serialized
+    
     private $_role;
     
-    // "loaded" means the reference that is consistent 
-    // with the current database state
+    /**
+     * The persister that is consistent with the current database state
+     * @var Xyster_Orm_Persister_Collection_Interface
+     */
     private $_loadedPersister;
+    /**
+     * The key that is consistent with the current database state
+     * @var mixed
+     */
     private $_loadedKey;
     
-    // ATTRIBUTES USED ONLY DURING FLUSH CYCLE
-    
-    // during flush, we navigate the object graph to
-    // collections and decide what to do with them
     private $_reached;
     private $_processed;
     private $_update;
     private $_remove;
     private $_recreate;
-    // if we instantiate a collection during the flush() process,
-    // we must ignore it for the rest of the flush()
     private $_ignore;
     
-    // "current" means the reference that was found during flush() 
+    /**
+     * The persister that was assigned during the flush process
+     *  
+     * @var Xyster_Orm_Persister_Collection_Interface
+     */
     private $_persister;
+    
+    /**
+     * The key that was assigned during the flush process
+     * @var mixed
+     */
     private $_key;
+    
+    /**
+     * Hidden constructor
+     */
+    private function __construct()
+    {  
+    }
+
+    /**
+     * Creates an entry for a newly wrapped collection or a dereferenced wrapper
+     * @param Xyster_Orm_Persister_Collection_Interface $persister
+     * @param Xyster_Orm_Collection_Interface $collection
+     * @return Xyster_Orm_Engine_CollectionEntry
+     */
+    public static function createForWrapped(Xyster_Orm_Persister_Collection_Interface $persister, Xyster_Orm_Collection_Interface $collection)
+    {
+        $entry = new self;
+        $entry->_ignore = false;
+        $collection->setDirty(false);
+        $entry->_snapshot = $persister->isMutable() ? 
+            $collection->getSnapshot($persister) : null;
+        $collection->setSnapshot($entry->_loadedKey, $entry->_role, $entry->_snapshot);
+        return $entry;
+    }
+    
+    /**
+     * Creates an entry for a collection just loaded from the database
+     * 
+     * @param Xyster_Orm_Persister_Collection_Interface $persister
+     * @param Xyster_Orm_Collection_Interface $collection
+     * @param mixed $key
+     * @param boolean $ignore
+     * @return Xyster_Orm_Engine_CollectionEntry
+     */
+    public static function createForLoaded(Xyster_Orm_Persister_Collection_Interface $persister, Xyster_Orm_Collection_Interface $collection, $key, $ignore)
+    {
+        $entry = new self;
+        $entry->_ignore = $ignore;
+        $entry->_loadedKey = $key;
+        $entry->_setLoadedPersister($persister);
+        $collection->setSnapshot($entry->_loadedKey, $entry->_role, null);
+        return $entry;
+    }
+    
+    /**
+     * Creates an entry for an uninitialized detached collection
+     * 
+     * @param Xyster_Orm_Persister_Collection_Interface $persister
+     * @param mixed $key
+     * @return Xyster_Orm_Engine_CollectionEntry
+     */
+    public static function createForUninitializedDetached(Xyster_Orm_Persister_Collection_Interface $persister, $key)
+    {
+        $entry = new self;
+        $entry->_ignore = false;
+        $entry->_loadedKey = $key;
+        $entry->_setLoadedPersister($persister);
+        return $entry;
+    }
     
     /**
      * Gets the persister
@@ -59,7 +126,7 @@ class Xyster_Orm_Engine_CollectionEntry
      */
     public function getPersister()
     {
-        
+        return $this->_persister;
     }
     
     /**
@@ -69,7 +136,7 @@ class Xyster_Orm_Engine_CollectionEntry
      */
     public function getKey()
     {
-        
+        return $this->_key;
     }
     
     /**
@@ -79,7 +146,7 @@ class Xyster_Orm_Engine_CollectionEntry
      */
     public function getLoadedKey()
     {
-        
+        $this->_loadedKey;
     }
     
     /**
@@ -88,7 +155,7 @@ class Xyster_Orm_Engine_CollectionEntry
      */
     public function getLoadedPersister()
     {
-        
+        return $this->_loadedPersister;
     }
     
     /**
@@ -100,7 +167,11 @@ class Xyster_Orm_Engine_CollectionEntry
      */
     public function getOrphans($entityName, Xyster_Orm_Collection_Interface $collection)
     {
-        
+        if ( $this->_snapshot === null ) {
+            require_once 'Xyster/Orm/Exception.php';
+            throw new Xyster_Orm_Exception('No collection snapshot to determine orphans');
+        }
+        return $collection->getOrphans($this->_snapshot, $entityName);
     }
     
     /**
@@ -110,7 +181,7 @@ class Xyster_Orm_Engine_CollectionEntry
      */
     public function getRole()
     {
-        
+        return $this->_role;
     }
 
     /**
@@ -120,7 +191,7 @@ class Xyster_Orm_Engine_CollectionEntry
      */
     public function getSnapshot()
     {
-        
+        return $this->_snapshot;
     }
     
     /**
@@ -130,7 +201,7 @@ class Xyster_Orm_Engine_CollectionEntry
      */
     public function isDereferenced()
     {
-        
+        return $this->getLoadedKey() === null;
     }
     
     /**
@@ -140,7 +211,7 @@ class Xyster_Orm_Engine_CollectionEntry
      */
     public function shouldRecreate()
     {
-        
+        return $this->_recreate;
     }
     
     /**
@@ -150,7 +221,7 @@ class Xyster_Orm_Engine_CollectionEntry
      */
     public function shouldRemove()
     {
-        
+        return $this->_remove;
     }
     
     /**
@@ -160,17 +231,19 @@ class Xyster_Orm_Engine_CollectionEntry
      */
     public function shouldUpdate()
     {
-        
+        return $this->_update;
     }
     
     /**
-     * Whether the collection is in ignore mode(?)
+     * Whether the collection can be ignored
+     * 
+     * A collection can be ignored if it was created during the flush process
      * 
      * @return boolean
      */
-    public function isIgnore()
+    public function canIgnore()
     {
-        
+        return $this->_ignore;
     }
     
     /**
@@ -180,7 +253,17 @@ class Xyster_Orm_Engine_CollectionEntry
      */
     public function isProcessed()
     {
-        
+        return $this->_processed;
+    }
+    
+    /**
+     * Whether the collection has been reached
+     * 
+     * @return boolean
+     */
+    public function isReached()
+    {
+        return $this->_reached;
     }
     
     /**
@@ -191,7 +274,20 @@ class Xyster_Orm_Engine_CollectionEntry
      */
     public function isSnapshotEmpty(Xyster_Orm_Collection_Interface $collection)
     {
-        
+        return $collection->isInitialized() &&
+            ($this->getLoadedPersister() === null ||
+                $this->getLoadedPersister()->isMutable()) &&
+            $collection->isSnapshotEmpty($this->getSnapshot());
+    }
+    
+    /**
+     * Called after an action is executed
+     * 
+     * @param Xyster_Orm_Collection_Interface $collection
+     */
+    public function postAction(Xyster_Orm_Collection_Interface $collection)
+    {
+        $lkey = $this->getKey();
     }
     
     /**
@@ -202,7 +298,14 @@ class Xyster_Orm_Engine_CollectionEntry
      */
     public function postFlush(Xyster_Orm_Collection_Interface $collection)
     {
-        
+        if ( $this->_ignore ) {
+            $this->_ignore = false;
+        } else if ( !$this->_processed ) {
+            require_once 'Xyster/Orm/Exception.php';
+            throw new Xyster_Orm_Exception('Collection not processed during flush');
+        }
+        $collection->setSnapshot($this->_loadedKey, $this->_role,
+            $this->_snapshot);
     }
     
     /**
@@ -213,7 +316,11 @@ class Xyster_Orm_Engine_CollectionEntry
      */
     public function postInit(Xyster_Orm_Collection_Interface $collection)
     {
-        
+        $persister = $this->_loadedPersister;
+        $snap = $persister->isMutable() ?
+            $collection->getSnapshot($persister) : null;
+        $collection->setSnapshot($this->_loadedKey, $this->_role,
+            $this->_snapshot); 
     }
     
     /**
@@ -224,7 +331,19 @@ class Xyster_Orm_Engine_CollectionEntry
      */
     public function preFlush(Xyster_Orm_Collection_Interface $collection)
     {
-        
+        $persister = $this->_loadedPersister;
+        $nonMutable = $collection->isDirty() && $persister != null &&
+            !$persister->isMutable();
+        if ( $nonMutable ) {
+            require_once 'Xyster/Orm/Exception.php';
+            throw new Xyster_Orm_Exception('Changed an immutable collection');
+        }
+        $this->_setDirty($collection);
+        $this->setUpdate(false)
+            ->setRemove(false)
+            ->setRecreate(false)
+            ->setReached(false)
+            ->setProcessed(false);
     }
     
     /**
@@ -235,7 +354,8 @@ class Xyster_Orm_Engine_CollectionEntry
      */
     public function setKey( $key )
     {
-        
+        $this->_key = $key;
+        return $this;
     }
     
     /**
@@ -246,7 +366,8 @@ class Xyster_Orm_Engine_CollectionEntry
      */
     public function setPersister( Xyster_Orm_Persister_Collection_Interface $persister )
     {
-        
+        $this->_persister = $persister;
+        return $this;
     }
     
     /**
@@ -257,7 +378,8 @@ class Xyster_Orm_Engine_CollectionEntry
      */
     public function setRecreate( $flag = true )
     {
-        
+        $this->_recreate = $flag;
+        return $this;
     }
     
     /**
@@ -268,7 +390,8 @@ class Xyster_Orm_Engine_CollectionEntry
      */
     public function setRemove( $flag = true )
     {
-        
+        $this->_remove = $flag;
+        return $this;
     }
     
     /**
@@ -279,7 +402,8 @@ class Xyster_Orm_Engine_CollectionEntry
      */
     public function setUpdate( $flag = true )
     {
-        
+        $this->_update = $flag;
+        return $this;
     }
     
     /**
@@ -290,7 +414,8 @@ class Xyster_Orm_Engine_CollectionEntry
      */
     public function setProcessed( $flag = true )
     {
-        
+        $this->_processed = $flag;
+        return $this;
     }
     
     /**
@@ -301,7 +426,8 @@ class Xyster_Orm_Engine_CollectionEntry
      */
     public function setReached( $flag = true )
     {
-        
+        $this->_reached = $flag;
+        return $this;
     }
     
     /**
@@ -312,6 +438,36 @@ class Xyster_Orm_Engine_CollectionEntry
      */
     public function setRole( $role )
     {
-        
+        $this->_role = $role;
+        return $this;
+    }
+    
+    /**
+     * Determines if a collection is dirty
+     * 
+     * @param Xyster_Orm_Collection_Interface $collection
+     */
+    protected function _setDirty(Xyster_Orm_Collection_Interface $collection)
+    {
+        $persister = $this->_loadedPersister;
+        $force = $collection->isInitialized() && !$collection->isDirty() &&
+            $persister != null && $persister->isMutable() &&
+            ($collection->isDirectAccess() ||
+                $persister->getElementType()->isMutable()) &&
+            !$collection->equalsSnapshot($persister);
+        if ( $force ) {
+            $collection->setDirty(true);
+        }
+    }
+    
+    /**
+     * Sets the loaded persister
+     * 
+     * @param Xyster_Orm_Persister_Collection_Interface $persister
+     */
+    protected function _setLoadedPersister(Xyster_Orm_Persister_Collection_Interface $persister = null)
+    {
+        $this->_loadedPersister = $persister;
+        $this->setRole($persister === null ? null : $persister->getRole());
     }
 }
